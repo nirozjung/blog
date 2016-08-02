97922d285c68af5cf8dd69db8f12ea5e1def212d
nirozjung <zorinzung@gmail.com>
2016-08-02 13:54:50 +0200

    Stucked at Association between Comment and Posts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ActiveRecord::Base
-  belongs_to :post
+  belongs_to :post, :user
+  validates_presence_of :post_id, :body
 end
